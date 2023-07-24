414036c01638a73d1aec396d80edb7ec9adc7f86
ahusu <su.aust@gmail.com>
2023-07-24 16:29:39 -0700

    added webpack to try to transpile everything.  built out server a bit to log, and serve static files

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -6,7 +6,12 @@ export default function LandingPage() {
 
   return (
     <div>
-      <h1>This is the landing page</h1>
+      <h1>Welcome to Daxis! Click add a disc to get started!</h1>
+      <div className='flex'>
+        <div onClick={()=>{}}>
+          <h3> Click here to add a disc!</h3>
+        </div>
+      </div>
     </div>
   )
 }
